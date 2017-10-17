167fff1c785d915315ab91392e36eabe4c1996a4
Kuznetsov Denis <kuznetsov.den.p@gmail.com>
2017-10-17 18:35:31 +0300

    Update handler_functions.py

--- handler_functions.py
+++ handler_functions.py
@@ -5,7 +5,7 @@ import pysrt
 import re
 
 
-__all__ = ['filter_function_ted','filter_function_ted_for_split_tokens']
+__all__ = ['filter_function_ted', 'filter_function_ted_for_split_tokens', 'wikiextractor_handler']
 
 def filter_function_ted(subs):
     text = []
@@ -29,6 +29,20 @@ def filter_function_ted_for_split_tokens(subs):
         if filtered[-1] in ['.', '?', '!']:
             filtered = filtered + '\n'
         # filtered = re.sub(r"([\w/'+$\s-]+|[^\w/'+$\s-]+)\s*", r"\1 ", filtered)
+        filtered = re.sub(r"([\*\"\'\\\/\|\{\}\[\]\;\:\<\>\,\.\!\?\*\(\)\"\«\»\“\”\@\#\%\$\„])", r" \1 ", filtered)
+        text.append(filtered)
+    text = text[1:-2]
+    text = re.sub(r' +', ' ', ' '.join(text))
+    return text
+
+def wikiextractor_handler(rawtextlines):
+    text = []
+    for line in rawtextlines:
+        filtered = re.sub(r'\n', ' ', line)
+        filtered = re.sub(r'\xa0', ' ', filtered)
+        filtered = re.sub(r'\([^)]*\)', ' ', filtered)
+        filtered = re.sub(r'\<\/doc\>', ' ', filtered)
+        filtered = re.sub(r'\<doc.*\>', ' ', filtered)
         filtered = re.sub(r"([\*\"\'\\\/\|\{\}\[\]\;\:\<\>\,\.\!\?\*\(\)\"\«\»\“\”\@\#\%\$\„])", r" \1 ", filtered)
         text.append(filtered)
     text = text[1:-2]
